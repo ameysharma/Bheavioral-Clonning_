4e6d36dfbbd58fd79f889570c99f27e0c72c499a
ameysharma <coolamey2@gmail.com>
2017-07-26 12:39:38 +0530

    Change 3

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # Behaviorial Cloning Project
 ###Data Collection
-*I used udacity generated data set to train my model.
-*Images were collected from all three cameras with following operations:-
-*Angle Adjustments of 0.22 were made to left(+.22) and Right Images(-.22)
-*Followed By fliping every image vertically and adjusting angles accordingly
-*All these data collection was done using generators to imporve memory utilization
+* I used udacity generated data set to train my model.
+* Images were collected from all three cameras with following operations:-
+* Angle Adjustments of 0.22 were made to left(+.22) and Right Images(-.22)
+* Followed By fliping every image vertically and adjusting angles accordingly
+* All these data collection was done using generators to imporve memory utilization
 
 ###Data Learing Model
-*In this model I first adjusted the images to range 0 to 1 using lamba function in keras.
-*Then I used 2 2D-Convolution neural networks of kernal 5X5 and filters 3 and 18 with Maxpooling function of 2 stides.
-*This was then followed by 3X3 Kernals with filiter 36 and 64 along with Maxpooling function
-*After performing this I used Dropout function to remove noise in the function
-*This was followed by flatten function and further followed by Dense function of 100,80 and 1
-*After Creating the model I used generators models for execution.
+* In this model I first adjusted the images to range 0 to 1 using lamba function in keras.
+* Then I used 2 2D-Convolution neural networks of kernal 5X5 and filters 3 and 18 with Maxpooling function of 2 stides.
+* This was then followed by 3X3 Kernals with filiter 36 and 64 along with Maxpooling function
+* After performing this I used Dropout function to remove noise in the function
+* This was followed by flatten function and further followed by Dense function of 100,80 and 1
+* After Creating the model I used generators models for execution.
 *Note:- Learning Rate= 0.0008, Losses =0.0208, Validation Losses=0.0221
 
-####Note: Model Suggestion taken from Nvdia Model. Moreover would like to thank  my mentor and forums mentor in helping me complete this project.
+####Note: Model Suggestion taken from Nvdia Model. Moreover, I would like to thank  my mentor and forums mentors in helping me complete this project.
--- model.py
+++ model.py
@@ -4,13 +4,13 @@ Created
 
 @author: amey sharma
 """
-
+ 
 import os
 import csv
 from sklearn.utils import shuffle
 
 # Data Collection
-
+# Reading Total number of Lines in an excel file 
 samples = []
 with open('data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
@@ -32,6 +32,8 @@ center_images=[]
 right_images=[]
 left_images=[]
 
+# Using Generators and collecting steering measurements of the images
+ 
 def generator(images, batch_size=128):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
@@ -42,6 +44,9 @@ def generator(images, batch_size=128):
             images = []
             angles = []
             for batch_sample in batch_samples:
+                
+                # Collecting steering angles and image data for central camera
+                # Moreover, Also flipping the images vertically .
                 filename=batch_sample[0].split('/')[-1]
                 current_path='data/IMG/'+filename
                 name = current_path
@@ -52,7 +57,9 @@ def generator(images, batch_size=128):
                 angles.append(angle)
                 images.append(cv2.flip(image,1))
                 angles.append(-1*angle)
-                #left image data with angle
+                
+                # Collecting left image data with angle improvement of +0.22 and
+                # Also flipping the image and adjusting the angles accordingly
                 filename=batch_sample[1].split('/')[-1]
                 current_path='data/IMG/'+filename
                 name = current_path
@@ -63,7 +70,9 @@ def generator(images, batch_size=128):
                 angles.append(angle+0.22)
                 images.append(cv2.flip(image,1))
                 angles.append(-1*(angle+0.22))
-                #Right image data
+                
+                #Collecting Right image data with angle improvement of -0.22
+                #Also flipping the image and adjusting angle accordingly.
                 filename=batch_sample[2].split('/')[-1]
                 current_path='data/IMG/'+filename
                 name = current_path
@@ -77,13 +86,14 @@ def generator(images, batch_size=128):
              
 
                 
-
-            # trim image to only see section with road
+            
+            # Generating Training Datasets 
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
             yield sklearn.utils.shuffle(X_train, y_train)
-             # compile and train the model using the generator function
+
+# compiling  and training 
 
 train_generator = generator(train_samples, batch_size=128)
 validation_generator = generator(validation_samples, batch_size=128)
@@ -96,26 +106,39 @@ from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras import optimizers
+
 model=Sequential()
+# Adjusting Image  data values to -1 to 1 .
 model.add(Lambda(lambda x: x/127.5 - 1,
         input_shape=(160, 320, 3)))
+#Cropping 70px from the Top and 20px from bottom
 model.add(Cropping2D(cropping=((70,20), (0,0))))
+#Using Convolution Layers of 5X5 Kernals and Filters 3 folllowed by 18.
+# Moreover Using Maxpool Function with 2 strides
 model.add(Convolution2D(3,5,5,activation='relu'))
 model.add(MaxPooling2D((2,2),strides=(2,2)))
 model.add(Convolution2D(18,5,5,activation='relu'))
 model.add(MaxPooling2D((2,2),strides=(2,2)))
+#Using Convolution Layers of 3X3 Kernals and Filters 36 folllowed by 64.
+# Moreover Using Maxpool Function.
 model.add(Convolution2D(36,3,3,activation='relu'))
 model.add(MaxPooling2D())
 model.add(Convolution2D(64,3,3,activation='relu'))
 model.add(MaxPooling2D())
+#Using A Dropout Rate of 0.5
 model.add(Dropout(0.5))
+#Flattening The 2d Dimensional image.
 model.add(Flatten())
+#Uing Dense model to Reduce the calues finally to 1
 model.add(Dense(100))
 model.add(Dense(80))
 model.add(Dense(1))
 #Setting the learning rate for the model
 adam=optimizers.adam(lr=0.0008)
+#Compiling the model
 model.compile(loss='mse',optimizer=adam)
+#Calling Generators Function Fo Training and Validation
 model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator,nb_val_samples=len(validation_samples), nb_epoch=20)
+#After 20 epoches Saving the model in "model.h5" file and "model.json" file.
 model.save('model.h5')
 model.save('model.json')
